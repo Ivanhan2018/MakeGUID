e67bf6dd5b05445be8f9427b99a865e0cf99b694
DESKTOP-7J2K9DH\001 <hanxiaohua1701@dingtalk.com>
2021-07-08 18:09:29 +0800

    调用Game.Utils.dll的加解密工具

---
+++ tool2021/tool2021/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Game.Utils;
+
+namespace tool2021
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string strSrc = this.textBox1.Text;
+            string str1 = DES.Encrypt(strSrc, "nJxPx8^#b*2Wnn^wUzQOxP0Q%C5MI*uE");
+            this.textBox2.Text=str1;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string strSrc = this.textBox1.Text;
+            string str1 = DES.Decrypt(strSrc, "nJxPx8^#b*2Wnn^wUzQOxP0Q%C5MI*uE");
+            this.textBox2.Text = str1;
+        }
+    }
+}
